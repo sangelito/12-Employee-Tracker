df0ee0f3c5ec081077cc3f4979ecf847bda51028
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 15:47:45 -0500

    employee seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -10,7 +10,7 @@ VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
 
-SELECT * FROM department
+SELECT * FROM department;
 
 --roles
 INSERT INTO role (title, salary, department_id)
@@ -24,4 +24,24 @@ VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 250000, 4);
 
-SELECT * FROM roles
+SELECT * FROM roles;
+
+-- employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Adams", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Tyson", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Allan", "Watts", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Maria", "Zardoya", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Julian", "Casablancas", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Summer", "Day", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dan", "Auerbach", 1, 2);
+
+SELECT * FROM employee;
