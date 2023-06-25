f88df716f0f638d6008056bfd5d9e6d574c47723
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 15:37:36 -0500

    department seed

---
+++ sql/seed.sql
@@ -0,0 +1,12 @@
+USE work_db;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+SELECT * FROM department
