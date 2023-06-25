b71d3aadc2db8a5c52bd0b0c23e37108b4a6cad3
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 15:32:07 -0500

    creating tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,3 +4,25 @@ CREATE DATABASE work_db;
 
 USE work_db; 
 
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
