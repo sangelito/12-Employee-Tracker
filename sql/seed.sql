e8467adfa7334d7d835f3e988c5d1b9080f60c76
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 15:42:27 -0500

    roles seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,5 +1,6 @@
 USE work_db;
 
+--departments 
 INSERT INTO department (name)
 VALUES ("Sales");
 INSERT INTO department (name)
@@ -10,3 +11,17 @@ INSERT INTO department (name)
 VALUES ("Legal");
 
 SELECT * FROM department
+
+--roles
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesman", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Financial Analyst", 120000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 250000, 4);
+
+SELECT * FROM roles
