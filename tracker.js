f4e21d0a08d7e8e5e4506718e77a5852d3f45a15
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 14:59:16 -0500

    connection

--- tracker.js
+++ tracker.js
@@ -1,3 +1,18 @@
 const mysql = require('mysql');
 const inquirer = require("inquirer");
 const table = require("console.table");
+
+//connection 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "123Pass!",
+    database: "work_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    askQuestions();
+});
