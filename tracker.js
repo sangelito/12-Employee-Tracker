9c61653db1fdf075024a8f656d627f91f1d7821e
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-25 18:12:32 -0500

    add dep function and prompt

--- tracker.js
+++ tracker.js
@@ -115,3 +115,18 @@ function addEmployee() {
         })
     })
 }
+
+// add department function and prompts
+function addDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        name: "department",
+        message: "What is the department that you want to add?"
+    }, ]).then(function(res) {
+        connection.query('INSERT INTO department (name) VALUES (?)', [res.department], function(err, data) {
+            if (err) throw err;
+            console.table("Successfully Inserted");
+            askQuestions();
+        })
+    })
+}
